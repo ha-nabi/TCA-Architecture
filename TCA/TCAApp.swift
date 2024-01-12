97eab576ac349ecf6e62096d6ae5ad2f91f1def2
ciu <112779139+kangciu@users.noreply.github.com>
2024-01-12 13:56:16 +0900

    Initial Commit

---
+++ TCA/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  TCA
+//
+//  Created by 강치우 on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
