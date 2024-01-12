181431c5356f7057914bb1f6c16911ce638b2f0e
ciu <112779139+kangciu@users.noreply.github.com>
2024-01-12 17:00:53 +0900

    TCA 조오오오오옹나어렵네 ㅠ.ㅠ

---
+++ TCA/CounterView.swift
@@ -0,0 +1,139 @@
+//
+//  ContentView.swift
+//  TCA
+//
+//  Created by 강치우 on 1/12/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+struct CounterFeature {
+    struct State: Equatable { // 변수 선언하는 곳
+        var count = 0
+        var fact: String?
+        var isLoading = false
+        var isTimerRunning = false
+    }
+    
+    enum Action { // 큰 함수의 틀을 만드는 곳
+        case decrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
+        case incrementButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
+    }
+    
+    enum CancelID { case timer }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in // 함수의 로직을 담당하는 곳
+            switch action {
+            case .decrementButtonTapped:
+                state.count -= 1
+                state.fact = nil
+                return .none
+                
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+                
+            case let .factResponse(fact):
+                state.fact = fact
+                state.isLoading = false
+                return .none
+                
+            case .incrementButtonTapped:
+                state.count += 1
+                state.fact = nil
+                return .none
+                
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+                
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
+            }
+        }
+    }
+}
+
+struct CounterView: View {
+    let store: StoreOf<CounterFeature>
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                Text("\(viewStore.count)")
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                HStack {
+                    Button("-") {
+                        viewStore.send(.decrementButtonTapped)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                    
+                    Button("+") {
+                        viewStore.send(.incrementButtonTapped)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                }
+                
+                Button(viewStore.isTimerRunning ? "Stop timer" : "Start timer") {
+                    viewStore.send(.toggleTimerButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                Button("Fact") {
+                    viewStore.send(.factButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            }
+        }
+    }
+}
+
+struct CounterPreview: PreviewProvider {
+    static var previews: some View {
+        CounterView(
+            store: Store(initialState: CounterFeature.State()) {
+                CounterFeature()
+            }
+        )
+    }
+}
--- TCA/TCAApp.swift
+++ TCA/TCAApp.swift
@@ -5,13 +5,20 @@
 //  Created by 강치우 on 1/12/24.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
+
 @main
-struct TCAApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct MyApp: App {
+  static let store = Store(initialState: CounterFeature.State()) {
+    CounterFeature()
+      ._printChanges()
+  }
+  
+  var body: some Scene {
+    WindowGroup {
+      CounterView(store: MyApp.store)
     }
+  }
 }
