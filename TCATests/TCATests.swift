899042bd8f7a4c9bb58475f8eabab0480f781f0d
ciu <112779139+kangciu@users.noreply.github.com>
2024-01-12 18:43:22 +0900

    add: 테스트코드 추가

---
+++ TCATests/TCATests.swift
@@ -0,0 +1,50 @@
+//
+//  TCATests.swift
+//  TCATests
+//
+//  Created by 강치우 on 1/12/24.
+//
+
+
+import ComposableArchitecture
+import XCTest
+
+@MainActor
+final class TCATests: XCTestCase {
+    func testCounter() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        
+        await store.send(.incrementButtonTapped) {
+            $0.count = 1
+        }
+        await store.send(.decrementButtonTapped) {
+            $0.count = 0
+        }
+    }
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+    
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
