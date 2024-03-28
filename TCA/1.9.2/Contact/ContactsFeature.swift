17f2eb48133ac8a6bb1ef5fddbca809e0575485d
kangciu <a2849535@gmail.com>
2024-03-28 15:29:25 +0900

    [1.9.2] Domain Modeling Refactor

--- TCA/1.9.2/Contact/ContactsFeature.swift
+++ TCA/1.9.2/Contact/ContactsFeature.swift
@@ -17,16 +17,14 @@ struct Contact: Equatable, Identifiable {
 struct ContactsFeature {
     @ObservableState
     struct State: Equatable {
-        @Presents var addContact: AddContactFeature.State?
-        @Presents var alert: AlertState<Action.Alert>?
         var contacts: IdentifiedArrayOf<Contact> = []
+        @Presents var destination: Destination.State?
     }
     
     enum Action {
         case addButtonTapped
-        case addContact(PresentationAction<AddContactFeature.Action>)
-        case alert(PresentationAction<Alert>)
         case deleteButtonTapped(id: Contact.ID)
+        case destination(PresentationAction<Destination.Action>)
         
         enum Alert: Equatable {
             case confirmDeletion(id: Contact.ID)
@@ -37,41 +35,47 @@ struct ContactsFeature {
         Reduce { state, action in
             switch action {
             case .addButtonTapped:
-                state.addContact = AddContactFeature.State(
-                    contact: Contact(id: UUID(), name: "")
+                state.destination = .addContact(
+                    AddContactFeature.State(
+                        contact: Contact(id: UUID(), name: "")
+                    )
                 )
                 return .none
-                
-            case let .addContact(.presented(.delegate(.saveContact(contact)))):
+        
+            case let .destination(.presented(.addContact(.delegate(.saveContact(contact))))):
                 state.contacts.append(contact)
                 return .none
                 
-            case .addContact:
-                return .none
-                
-            case let .alert(.presented(.confirmDeletion(id: id))):
+            case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
                 state.contacts.remove(id: id)
                 return .none
                 
-            case .alert:
+            case .destination:
                 return .none
                 
             case let .deleteButtonTapped(id: id):
-                state.alert = AlertState {
-                    TextState("정말 삭제하시겠어요?")
-                } actions: {
-                    ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
-                        TextState("삭제")
+                state.destination = .alert(
+                    AlertState {
+                        TextState("정말 삭제하시겠어요?")
+                    } actions: {
+                        ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                            TextState("삭제")
+                        }
                     }
-                }
+                )
                 return .none
             }
         }
         // .ifLet 하위 작업이 시스템에 들어올 때 하위 리듀서를 실행하고 모든 작업에서 상위 리듀서를 실행하는 새 리듀서가 생성됨.
         // 또한 하위 기능이 해제될 때 효과 취소 등을 자동으로 처리
-        .ifLet(\.$addContact, action: \.addContact) {
-            AddContactFeature()
-        }
-        .ifLet(\.$alert, action: \.alert)
+        .ifLet(\.$destination, action: \.destination)
+    }
+}
+
+extension ContactsFeature {
+    @Reducer(state: .equatable)
+    enum Destination {
+        case addContact(AddContactFeature)
+        case alert(AlertState<ContactsFeature.Action.Alert>)
     }
 }
